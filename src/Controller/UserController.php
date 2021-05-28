9e48a294a6bec61ea5f98536ff961caafea30475
amel105 <56436184+amel105@users.noreply.github.com>
2021-05-28 19:53:34 +0200

    first commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(): Response
+    {
+        $name = " Bassem";
+        $users =[$name,"Rabaa","Amal","chaima"];
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+            "name"=>$name,
+            'utilisateurs' => $users
+        ]);
+
+    }
+
+    /**
+     * @Route("/cv",name="cv")
+     */
+    public function test()
+    {
+        echo "test";
+        return new Response();
+
+    }
+
+    /**
+     * @Route("/addfromangular",name="add",methods={"post"})
+     */
+    public function add(Request $request)
+    {
+    $resultat = json_decode($request->getContent(),true);
+    
+    $user = new User();
+    $user->setFirstName($resultat['amal'])
+        ->setPassword("dfn5454")
+        ->setEmail("amal@gmail.com")
+        ->setPhoneNumber('25609125');
+
+   $em = $this->container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+       $response = new Response();
+       $response->setStatusCode('200');
+       return $response;
+
+    }
+
+
+    /**
+     * @Route("/updateUser",name="update",methods={"post"})
+     */
+    public function updateUser(Request $request)
+    {
+        $resultat = json_decode($request->getContent(),true);
+          dd($resultat);
+        $em = $this->container->get('doctrine')->getManager();
+        $user = $em->getRepository(User::class)->findOneBy(array('Email' => $resultat['Email']));
+
+        if ($user){
+            $user->setFirstName('Cristiano');
+            $em->persist($user);
+            $em->flush();
+
+        }
+
+        $response = new Response();
+        $response->setStatusCode('200');
+        return $response;
+
+    }
+
+
+    /**
+     * @Route("/inscription",name="register",methods={"post"})
+     */
+    public function register(Request $request , UserPasswordEncoderInterface $encoder)
+    {
+        $resultat = json_decode($request->getContent(),true);
+
+        $em = $this->container->get('doctrine')->getManager();
+        $user = $em->getRepository(User::class)->findOneBy(array('Email' => $resultat['Email']));
+
+        if (!$user){
+        $user = new User();
+        $user->setFirstName($resultat['FirstName'])
+             ->setPassword($encoder->encodePassword($user,$resultat['Password']) ) 
+             ->setEmail($resultat['Email'])
+             ->setPhoneNumber($resultat['PhoneNumber']);
+
+        $em->persist($user);
+        $em->flush();
+
+            $response = new Response();
+            $response->setContent(json_encode(array(
+                'result' => 'Job done !',
+            )));
+            $response->setStatusCode(200    );
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+
+        //dd('user not found you can add new user');
+        }
+        $response = new Response();
+        $response->setContent(json_encode(array(
+            'result' => 'user already exist !',
+        )));
+        $response->setStatusCode(400    );
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+
+    }
+     /**
+     * @Route("/editProfil",name="edit_profil",methods={"post"})
+     */
+    public function editProfile(Request $request){
+        $result = json_decode($request->getContent(),true);
+        $em = $this->container->get('doctrine')->getManager();
+        $User = $em->getRepository(User::class)->findOneBy(array('id' => (int)$resultat['idUser']));
+        if($user){
+            //dd('here');
+           // dd($result['email']);
+            if($user->getEmail() == $resultat['Email']){
+               // dd('here');
+                $user->setFirstName($resultat['FirstName'])
+                ->setLastName($resultat['LastName'])
+                ->setPhoneNumber($resultat['PhoneNumber'])
+                ->setDescription($resultat['description'])
+                ->setCompany($resultat['company'])
+                ->setTitre($resultat['titre'])
+                ->setEmail($resultat['Email']);
+
+
+                $em->persist($user);
+                $em->flush();
+                  
+                    $response = new Response();
+                    $response->setContent(json_encode(array(
+                        'result' => 'Job done !',
+                    )));
+                    $response->setStatusCode(200    );
+                    $response->headers->set('Content-Type', 'application/json');
+                    return  $response;
+            }else{
+               
+                $us = $em->getRepository(User::class)->findOneBy(array('email' => $resultat['email']));
+               //dd($us);
+                if($us){
+                    // if user exist
+                    $response = new Response();
+                    $response->setContent(json_encode(array(
+                        'result' => 'user already exist !',
+                    )));
+                    $response->setStatusCode(400    );
+                    $response->headers->set('Content-Type', 'application/json');
+                    return  $response;
+            
+
+
+                }else{
+                    return new Response();
+
+                    // user not exist
+                    dd('user not exist');
+                }
+
+            }
+            //dd($user->getEmail());
+            //$us = $em->getRepository(User::class)->findOneBy(array('id' => $result['email']));
+
+        }
+        return new Response();
+       // dd($user);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * @Route("/getuser",name="get_user",methods={"post"})
+     */
+    public function getu(Request $request){
+        $resultat = json_decode($request->getContent(),true);
+        $em = $this->container->get('doctrine')->getManager();
+        $user = $em->getRepository(User::class)->findOneBy(array('Email' => $resultat['Email']));
+        $serializer = $this->container->get('serializer');
+        $user = $serializer->serialize($user, 'json');
+        return new JsonResponse ( $user );
+        
+//dd($user);
+//return new Response();
+    }
+
+
+    /**
+     * @Route("/getusers",name="get_users",methods={"get"})
+     */
+    public function getAllUsers(){
+        $em = $this->container->get('doctrine')->getManager();
+
+        $us = $em->getRepository(User::class)->findUser();
+        
+        $serializer = $this->container->get('serializer');
+         $itemSerializer = $serializer->serialize($us, 'json');
+       // dd($itemSerializer);
+         return new Response( $itemSerializer );
+     // $response = new Response($itemSerializer );
+       //  $response->setContent( $itemSerializer);
+      //   $response->setStatusCode(200    );
+     // $response->headers->set('Content-Type', 'application/json');
+//  return $response;
+       // return new Response();
+    }
+
+    
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+
+
+/**
+ * @ApiResource
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+
+#[ApiResource]
+class User implements  UserInterface 
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $First_Name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Last_Name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Phone_Number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Password;
+
+    
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     */
+    private $Job;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     */
+    private $Country;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     */
+    private $Experience;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     */
+    private $company;
+
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->First_Name;
+    }
+
+    public function setFirstName(string $First_Name): self
+    {
+        $this->First_Name = $First_Name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->Last_Name;
+    }
+
+    public function setLastName(string $Last_Name): self
+    {
+        $this->Last_Name = $Last_Name;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?int
+    {
+        return $this->Phone_Number;
+    }
+
+    public function setPhoneNumber(int $Phone_Number): self
+    {
+        $this->Phone_Number = $Phone_Number;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+    public function eraseCredentials(): ?string{
+        return null;
+    }
+    public function getRoles(){
+        return array('ROLE_USER');
+    }
+    public function getSalt():?string
+    {
+        return null;
+        }
+    public function getUsername(){
+        return $this->Email;
+    }
+
+    
+
+    
+       
+
+    public function getJob(): ?string
+    {
+        return $this->Job;
+    }
+
+    public function setJob(string $Job): self
+    {
+        $this->Job = $Job;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->Country;
+    }
+
+    public function setCountry(string $Country): self
+    {
+        $this->Country = $Country;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getExperience(): ?string
+    {
+        return $this->Experience;
+    }
+
+    public function setExperience(string $Experience): self
+    {
+        $this->Experience = $Experience;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(string $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+   
+
+    
+
+
+}
